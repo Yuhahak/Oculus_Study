63911e083b2ba394b4489d1633759cded5c741a0
Yuhahak <dbwogkr1214@naver.com>
2023-09-03 00:45:05 +0900

    플레이어 움직임
    카메라 제어, 움직임
    적 피격 판정

--- Oculus/Assets/02_Scirpts/Enemy/EnemyOne.cs
+++ Oculus/Assets/02_Scirpts/Enemy/EnemyOne.cs
@@ -1,28 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyOne : EnemyBase
 {
+    public static EnemyOne instance;
+
+    [Header("Setting")]
+    public Transform target;
+    NavMeshAgent nav;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
+        EnemyOne.instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nav.SetDestination(target.position);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void EnemyTakeDamage(int PlusDamage)
     {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            enemy_Hp -= player.damage;
-        }
+        enemy_Hp -= (player.damage + PlusDamage);
     }
+
 }
---
+++ Oculus/Assets/02_Scirpts/Gun/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Ground"))
+        {
+            EnemyOne.instance.EnemyTakeDamage(0);
+            Destroy(gameObject);
+        }
+    }
+}
