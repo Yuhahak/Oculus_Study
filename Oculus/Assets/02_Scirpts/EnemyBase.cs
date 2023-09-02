190e44c2ad228b2a69285fd6808df690d20dadf1
Yuhahak <dbwogkr1214@naver.com>
2023-09-02 15:54:20 +0900

    정리_2학기_시작

---
+++ Oculus/Assets/02_Scirpts/EnemyBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    [Header("Stat")]
+    public int enemy_Hp;
+    public float enemy_MoveSpeed;
+    public int enemy_Damage;
+}
---
+++ Oculus/Assets/02_Scirpts/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Stat")]
+    public int hp;
+    public float moveSpeed;
+    public int damage;
+}
