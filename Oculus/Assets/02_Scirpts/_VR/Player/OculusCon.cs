b73abc49f614931a0b11e97f909101342fbd5d3d
Yuhahak <dbwogkr1214@naver.com>
2023-09-18 15:34:13 +0900

    .
    .

--- Oculus/Assets/02_Scirpts/_VR/Player/OculusCon.cs
+++ Oculus/Assets/02_Scirpts/_VR/Player/OculusCon.cs
@@ -10,125 +10,132 @@ public class OculusCon : MonoBehaviour
     [HideInInspector] public OVRInput.Controller controllerL; //왼손 컨트롤러
     [HideInInspector] public OVRInput.Controller controllerR; //오른손 컨트롤러
 
-    public Text logText; //출력 메시지
-
     [HideInInspector] public OVRGrabber grabberL;
     [HideInInspector] public OVRGrabber grabberR;
 
     public GameObject grabL;
     public GameObject grabR;
 
-    
-
-
+    public GameObject Player;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(grabberL.m_grabbedObj)
+
+
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))  //오른손 트리거를 눌렀을 때
         {
-            grabL = grabberL.m_grabbedObj.gameObject;
-        }
-        else
-        {
-            grabL = null;
-        }
-        if(grabberR.m_grabbedObj)
-        {
-            grabR = grabberR.m_grabbedObj.gameObject;
-        }
-        else
-        {
-            grabR = null;
+
         }
 
 
 
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
-        {
 
-           
-            
-        }
-        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
-        {
-            if (grabR)
-            {
-                switch(grabR.transform.name)
-                {
-                    case "Gun":
-                        {
-                            grabR.GetComponent<BulletCreate>().CheckGun();
-                            break;
-                        }
-                    case "Game_Start":
-                        {
-                            UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Scene");
-                            break;
-                        }
-                    case "KeyPad_Col":
-                        {
-                            Keypad.s.KeyPad_Col_Open = true;
-                            break;
-                        }
-                    case "KeyPad_1":
-                        KeyPadOpen.instance.Ans.text += "1";
-                        break;
+        //if(grabberL.m_grabbedObj)
+        //{
+        //    grabL = grabberL.m_grabbedObj.gameObject;
+        //}
+        //else
+        //{
+        //    grabL = null;
+        //}
+        //if(grabberR.m_grabbedObj)
+        //{
+        //    grabR = grabberR.m_grabbedObj.gameObject;
+        //}
+        //else
+        //{
+        //    grabR = null;
+        //}
 
-                }
-            }
-        }
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
-        {
-        }
 
-        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
-        {
-        }
 
-        if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
-        {
-            Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        }
+        //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        //    {
 
-        if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))
-        {
-            Vector2 pos = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-        }
 
-        //A버튼 (오른손)
 
-        if (OVRInput.GetDown(OVRInput.Button.One))
-        {
-        }
+        //    }
+        //    if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        //    {
+        //        if (grabR)
+        //        {
+        //            switch(grabR.transform.name)
+        //            {
+        //                case "Gun":
+        //                    {
+        //                        grabR.GetComponent<BulletCreate>().CheckGun();
+        //                        break;
+        //                    }
+        //                case "Game_Start":
+        //                    {
+        //                        UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Scene");
+        //                        break;
+        //                    }
+        //                case "KeyPad_Col":
+        //                    {
+        //                        Keypad.s.KeyPad_Col_Open = true;
+        //                        break;
+        //                    }
+        //                case "KeyPad_1":
+        //                    KeyPadOpen.instance.Ans.text += "1";
+        //                    break;
 
-        //B버튼 (오른손)
-        if (OVRInput.GetDown(OVRInput.Button.Two))
-        {
-        }
+        //            }
+        //        }
+        //    }
+        //    if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        //    {
+        //    }
 
-        //X버튼 (왼손)
-        if (OVRInput.GetDown(OVRInput.Button.Three))
-        {
-        }
+        //    if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        //    {
+        //    }
 
-        //Y버튼 (왼손)
-        if (OVRInput.GetDown(OVRInput.Button.Four))
-        {
-        }
+        //    if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
+        //    {
+        //        Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        //    }
 
-        //스타트 버튼 (왼손)
+        //    if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))
+        //    {
+        //        Vector2 pos = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        //    }
 
-        if (OVRInput.GetDown(OVRInput.Button.Start))
-        {
-        }
+        //    //A버튼 (오른손)
+
+        //    if (OVRInput.GetDown(OVRInput.Button.One))
+        //    {
+        //    }
+
+        //    //B버튼 (오른손)
+        //    if (OVRInput.GetDown(OVRInput.Button.Two))
+        //    {
+        //    }
+
+        //    //X버튼 (왼손)
+        //    if (OVRInput.GetDown(OVRInput.Button.Three))
+        //    {
+        //    }
+
+        //    //Y버튼 (왼손)
+        //    if (OVRInput.GetDown(OVRInput.Button.Four))
+        //    {
+        //    }
+
+        //    //스타트 버튼 (왼손)
+
+        //    if (OVRInput.GetDown(OVRInput.Button.Start))
+        //    {
+        //    }
+        //}
+
     }
-
 }
